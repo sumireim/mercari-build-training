f282feb05e1be226e6d4ff82949406ed541f2151
23imamoto <sumire@g.ecc.u-tokyo.ac.jp>
2025-04-23 19:11:39 +0900

    テーブルの修正

--- go/db/items.sql
+++ go/db/items.sql
@@ -1,3 +1,8 @@
+CREATE TABLE IF NOT EXISTS categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,  
+    name TEXT NOT NULL                     
+);
+
 CREATE TABLE IF NOT EXISTS items (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
@@ -6,12 +11,5 @@ CREATE TABLE IF NOT EXISTS items (
     FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
 );
 
-
-CREATE TABLE IF NOT EXISTS categories (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,  
-    name TEXT NOT NULL                     
-);
-
 INSERT INTO categories (name) VALUES ('phone');
-
 INSERT INTO categories (name) VALUES ('fashion');
